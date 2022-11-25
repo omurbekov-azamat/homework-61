114b24b10b6a0504a87817a8892fdc5a7b509a87
omurbekov-azamat <avgusttin8@gmail.com>
2022-11-25 17:15:12 +0600

    commit-6; changed function fetchBorder;

--- src/components/CloseCountry/CloseCountry.tsx
+++ src/components/CloseCountry/CloseCountry.tsx
@@ -13,7 +13,7 @@ const CloseCountry: React.FC<Props> = ({alpha3Code}) => {
   const [country, setCountry] = useState<MoreAboutCountry | null>(null);
 
   const fetchBorder = useCallback(async (alpha3Code: string) => {
-
+    let closeCountries: string[] = [];
     const countryResponse = await axios.get<MoreAboutCountry>(countryUrl + alpha3Code);
 
     if (countryResponse.data.borders) {
@@ -22,22 +22,15 @@ const CloseCountry: React.FC<Props> = ({alpha3Code}) => {
         return borderCountries.data.name;
       });
 
-      const closeCountries = await Promise.all(promises);
-
-      setCountry({
-        flag: countryResponse.data.flag,
-        name: countryResponse.data.name,
-        capital: countryResponse.data.capital,
-        borders: closeCountries,
-      });
-    } else {
-      setCountry({
-        flag: countryResponse.data.flag,
-        name: countryResponse.data.name,
-        capital: countryResponse.data.capital,
-        borders: [],
-      });
+      closeCountries = await Promise.all(promises);
     }
+
+    setCountry({
+      flag: countryResponse.data.flag,
+      name: countryResponse.data.name,
+      capital: countryResponse.data.capital,
+      borders: closeCountries,
+    });
   }, []);
 
   useEffect(() => {
